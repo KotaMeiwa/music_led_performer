17f68a5eba0bf25b5fb037d2b00ca2ca75424f19
Kota Meiwa <128406796+KotaMeiwa@users.noreply.github.com>
2023-05-14 01:58:59 +0900

    range is narrowed

--- example/sub3/sub3.ino
+++ example/sub3/sub3.ino
@@ -10,8 +10,11 @@
 //抵抗と光センサを直列つなぎ。光センサ側を - へ。
 //抵抗 1K ohm
 //光センサ 明るい 0.5K ohm, 暗い 50K ohm
-const int g_val_min = 1024 * 1/3;   //明るい  0.5K / (0.5K + 1K) = 1/3
-const int g_val_max = 1024 * 50/51; //暗い  50K / (50K + 1K) = 50/51
+const int g_val_spec_min = 1024 * 1/3;   //明るい  0.5K / (0.5K + 1K) = 1/3
+const int g_val_spec_max = 1024 * 50/51; //暗い  50K / (50K + 1K) = 50/51
+
+const int g_val_min = 871;  //明るい
+const int g_val_max = 941;  //暗い
 const int g_val_step = (g_val_max - g_val_min)/4;
 const uint8_t g_leds[4] = {LED0, LED1, LED2, LED3};
 const uint8_t g_led_num = sizeof(g_leds)/sizeof(g_leds[0]);
@@ -33,7 +36,7 @@ void setup() {
 ///////////////////////////////////////
 void loop()
 {
-  int val  = analogRead(READ_PIN) - g_val_min;
+  int val  = max(analogRead(READ_PIN) - g_val_min, 0);
 
   //LED点灯数 最明るい1つ  最暗い4つ
   int level = val / g_val_step +1;
@@ -46,11 +49,14 @@ void loop()
   static MSG_CMD last_msg_cmd = MSG_CMD_UNK;
 
   //テンポラリ実装
-  //Level 2以下＝明るい ならRUN。
-  MSG_CMD msg_cmd = level<=2? MSG_CMD_RUN: MSG_CMD_STILL;
+  //Level 3以上＝暗い ならRUN。
+  MSG_CMD msg_cmd = 3<=level? MSG_CMD_RUN: MSG_CMD_STILL;
   msg_cmd!=last_msg_cmd? MP.Send(MSGIDs_CMD, last_msg_cmd=msg_cmd): 0;
 #endif  //SUBCORE
 
-  //Serial.printf("min=%d, max=%d, step=%d, val=%d, level=%d \n", g_val_min, g_val_max, g_val_step, val, level);
+#ifndef SUBCORE
+  Serial.printf("min=%d, max=%d, step=%d, val=%d, level=%d \n", g_val_min, g_val_max, g_val_step, val, level);
+#endif  //SUBCORE
+
   delay(500);
 }
